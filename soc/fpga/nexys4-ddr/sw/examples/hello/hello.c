22577b53e497a25d51ff466ae097b1d4a9f6f5b9
atesba <batuhanates04@gmail.com>
2020-03-01 18:22:10 +0300

    Hello World!

---
+++ soc/fpga/nexys4-ddr/sw/examples/hello/hello.c
@@ -0,0 +1,12 @@
+#include <stdint.h>
+
+#include "../../libs/soc/platform.h"
+#include "../../libs/soc/uart.h"
+
+static struct uart uart0;
+
+void main() {
+	uart_init(&uart0, (volatile void *) PLATFORM_UART_BASE);
+	uart_set_baudrate(&uart0, 115200, PLATFORM_CLK_FREQ);
+	uart_printf(&uart0, "Hello World!\r\n");
+}

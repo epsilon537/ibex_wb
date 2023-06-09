4bd0bd4ee8dd3b1027e15a94d6f58449af1d1b36
epsilon <ruben.lysens2@gmail.com>
2023-06-09 15:23:21 +0200

    Using XPM memory instance on Arty.

--- soc/fpga/arty-a7-35/rtl/wb_spramx32.sv
+++ soc/fpga/arty-a7-35/rtl/wb_spramx32.sv
@@ -17,6 +17,7 @@ module wb_spramx32
    logic [31:0]             ram_d;
    logic [31:0]             ram_q;
 
+`ifdef VERILATOR
    spramx32
      #(.size(size),
        .init_file(init_file))
@@ -27,6 +28,67 @@ module wb_spramx32
       .we   (ram_we),
       .d    (ram_d),
       .q    (ram_q));
+`else
+/*On the Arty, use XPM memory instances. This allows post-implementation updates of the 
+ *memory contents in the bitstream.*/
+xpm_memory_spram #(
+   .ADDR_WIDTH_A(addr_width),              // DECIMAL
+   .AUTO_SLEEP_TIME(0),           // DECIMAL
+   .BYTE_WRITE_WIDTH_A(8),       // DECIMAL
+   .ECC_MODE("no_ecc"),           // String
+   .MEMORY_INIT_PARAM("0"),       // String
+   .MEMORY_OPTIMIZATION("true"),  // String
+   .MEMORY_PRIMITIVE("auto"),     // String
+   .MEMORY_SIZE(size*8),          // DECIMAL, memory size in bits
+   .MESSAGE_CONTROL(0),           // DECIMAL
+   .READ_DATA_WIDTH_A(32),        // DECIMAL
+   .READ_LATENCY_A(1),            // DECIMAL
+   .READ_RESET_VALUE_A("0"),      // String
+   .USE_MEM_INIT(1),              // DECIMAL
+   .WAKEUP_TIME("disable_sleep"), // String
+   .WRITE_DATA_WIDTH_A(32),       // DECIMAL
+   .WRITE_MODE_A("read_first"),   // String
+   .RAM_DECOMP("power")           // String
+)
+xpm_memory_spram_inst (
+   .dbiterra(),             // 1-bit output: Status signal to indicate double bit error occurrence
+                                    // on the data output of port A.
+
+   .douta(ram_q),                   // READ_DATA_WIDTH_A-bit output: Data output for port A read operations.
+   .sbiterra(),             // 1-bit output: Status signal to indicate single bit error occurrence
+                                    // on the data output of port A.
+
+   .addra(ram_addr),                   // ADDR_WIDTH_A-bit input: Address for port A write and read operations.
+   .clka(wb.clk),                     // 1-bit input: Clock signal for port A.
+   .dina(ram_d),                     // WRITE_DATA_WIDTH_A-bit input: Data input for port A write operations.
+   .ena(ram_ce),                       // 1-bit input: Memory enable signal for port A. Must be high on clock
+                                    // cycles when read or write operations are initiated. Pipelined
+                                    // internally.
+
+   .injectdbiterra(1'b0), // 1-bit input: Controls double bit error injection on input data when
+                                    // ECC enabled (Error injection capability is not available in
+                                    // "decode_only" mode).
+
+   .injectsbiterra(1'b0), // 1-bit input: Controls single bit error injection on input data when
+                                    // ECC enabled (Error injection capability is not available in
+                                    // "decode_only" mode).
+
+   .regcea(1'b1),                 // 1-bit input: Clock Enable for the last register stage on the output
+                                    // data path.
+
+   .rsta(wb.rst),                     // 1-bit input: Reset signal for the final port A output register stage.
+                                    // Synchronously resets output port douta to the value specified by
+                                    // parameter READ_RESET_VALUE_A.
+
+   .sleep(1'b0),                   // 1-bit input: sleep signal to enable the dynamic power saving feature.
+   .wea(ram_we)                        // WRITE_DATA_WIDTH_A-bit input: Write enable vector for port A input
+                                    // data port dina. 1 bit wide when word-wide writes are used. In
+                                    // byte-wide write configurations, each bit controls the writing one
+                                    // byte of dina to address addra. For example, to synchronously write
+                                    // only bits [15-8] of dina when WRITE_DATA_WIDTH_A is 32, wea would be
+                                    // 4'b0010.
+);
+`endif
 
    assign ram_addr = wb.adr[addr_width + 1 : 2];
    assign ram_ce   = valid;
